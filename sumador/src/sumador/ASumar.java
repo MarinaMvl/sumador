50e5d4e30ee7b8ebd151ed2e3616a72a29d5f260
Maryna Ulasava <marynaulasava@gmail.com>
2020-03-12 22:15:59 +0100

    prueba con una cifra

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -7,9 +7,16 @@ public class ASumar {
 	
 	public ASumar() {
 		// TODO Auto-generated constructor stub
+		}
+	public String mostrar(String cad) {
 		
-		
+		if (cad.length() == 1) {
+            return (cad + " = " + cad);
+            
+		}
+		return null;
 	}
+	
 }
 	
 	
--- sumador/test/sumador/ASumarTest.java
+++ sumador/test/sumador/ASumarTest.java
@@ -6,7 +6,16 @@ import org.junit.Test;
 
 public class ASumarTest {
 
+	//Probar con un número de sólo un dígito.
 	
+		 @Test
+		public void testUnaCifra() {
+		        ASumar sumi = new ASumar();
+		        String resultado = sumi.mostrar("5");
+		        assertEquals("5 = 5",resultado );
+		    }
+		 
+		 
 	
 
 }
