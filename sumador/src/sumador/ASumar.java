f5452fc09a09cbaaed9cdc7e7fb96da61240ed64
Maryna Ulasava <marynaulasava@gmail.com>
2020-03-12 23:14:35 +0100

    javadoc generated

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,13 +1,26 @@
 package sumador;
 
 import java.util.*;
-
+/**
+ * 
+ * @author Maryna Ulasava
+ * el método ASumar suma los digitos de un numero y devuelve el resultado, si es negativo devuelve 
+ * la cadena vacía.
+ *
+ */
 
 public class ASumar {
 	
 	public ASumar() {
 		// TODO Auto-generated constructor stub
 		}
+	
+	/**
+	 * 
+	 * @param cad : el numero recibido
+	 * @return devuelve el numero recibido, en el caso de tener mas de un digito, devuelve los sumandos y el resultado.
+	 */
+	
 	public String mostrar(String cad) {
 		
 		if (cad.length() == 1) {
