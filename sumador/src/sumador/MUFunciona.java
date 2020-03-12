472c49c39208775e3187f6472bc12a862a13c6b0
Maryna Ulasava <marynaulasava@gmail.com>
2020-03-12 23:27:07 +0100

    resultado

---
+++ sumador/src/sumador/MUFunciona.java
@@ -0,0 +1,19 @@
+package sumador;
+
+public class MUFunciona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ASumar s=new ASumar();
+		System.out.println(s.mostrar("5"));
+		System.out.println(s.mostrar("-9"));
+		System.out.println(s.mostrar("123"));
+		
+		
+		
+		
+	}
+
+}
